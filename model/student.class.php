57ae5953519ade46cb7b22e2be54bc9b7713c52f
Javito <dfj.11.11.1972@gmail.com>
2024-05-22 21:52:17 -0300

    Creamos las funciones get, edit, delete.

--- model/student.class.php
+++ model/student.class.php
@@ -30,7 +30,7 @@ class Student {
 
     public function addStudent(){
         //crear la consulta
-        $sql="INSERT INTO students (dni, surname, name, birthdate, phone, address, email, user, password, school) VALUES (".$this->dni.",'".$this->surname."','".$this->name."','".$this->birthdate."','".$this->phone."','".$this->address."','".$this->email."','".$this->.password"','".$this->.school"')";
+        $sql="INSERT INTO students (dni, surname, name, birthdate, phone, address, email, password, school) VALUES (".$this->dni.",'".$this->surname."','".$this->name."','".$this->birthdate."','".$this->phone."','".$this->address."','".$this->email."','".$this->password."','".$this->school."')";
         
         $this->conexion=new Database();
         $result= $this->conexion=query($sql);
@@ -41,15 +41,46 @@ class Student {
     }
 
     public function editStudent(){
+        $sql="UPDATE students SET dni=".$this->dni.", surname='".$this->surname."', name='".$this->name."', birthdate='".$this->birthdate."', phone='".$this->phone."', address='".$this->address."', email='".$this->email."', password='".$this->password."', school='".$this->school."' WHERE idStudent=".$this->idStudent;
+        
+        $this->conexion=new Database();
+        $result= $this->conexion=query($sql);
+        $this->conexion->close();
 
+        return $result;
     }
 
     public function deleteStudent(){
+        $sql="DELETE FROM students WHERE idStudent=".this->$idStudent;
 
+        $this->conexion=new Database();
+        $result= $this->conexion=query($sql);
+        $this->conexion->close();
+
+        return $result;
     }
 
     public function getStudent(){
+        $sql="SELECT * FROM students WHERE idStudent=".this->$idStudent;
 
+        $this->conexion=new Database();
+        $result= $this->conexion=query($sql);
+        $this->conexion->close();
+        if ($result){
+            if($row=$result->fetch_assoc)){
+                $this->dni=$row["dni"];
+                $this->surname=$row["surname"];
+                $this->name=$row["name"];
+                $this->birthdate=$row["birthdate"];
+                $this->phone=$row["phone"];
+                $this->address=$row["address"];
+                $this->email=$row["email"];
+                $this->password=$row["password"];
+                $this->school=$row["school"];
+                return true;
+            }
+        }
+        return false;
     }
 
     public function getAllStudents(){
