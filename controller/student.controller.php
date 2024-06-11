7180a30be7958f8d56aaf9c7b638756bf0fb591e
FalcoDavid <dfj.11.11.1972@gmail.com>
2024-06-11 14:30:35 -0300

    Update y delete

--- controller/student.controller.php
+++ controller/student.controller.php
@@ -20,8 +20,19 @@ if($operation=="add"){
     $student->setSchool($_REQUEST["school"]);
     $student->addStudent();
 }else if($operation=="update"){
-
-}else if($operation=="delete"){
+    $student->setDni($_REQUEST["dni"]);
+    $student->setSurname($_REQUEST["surname"]);
+    $student->setName($_REQUEST["name"]);
+    $student->setBirthdate($_REQUEST["birthdate"]);
+    $student->setPhone($_REQUEST["phone"]);
+    $student->setAddress($_REQUEST["address"]);
+    $student->setEmail($_REQUEST["email"]);
+    $student->setPassword($_REQUEST["password"]);
+    $student->setSchool($_REQUEST["school"]);
+    $student->editStudent();
+} else if($operation=="delete"){
+    $student->setDni($_REQUEST["dni"]); 
+    $student->deleteStudent();
     
 }
 
