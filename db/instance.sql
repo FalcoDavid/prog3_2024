4b886c928cff116482715c6ae069595bb803af9c
FalcoDavid <dfj.11.11.1972@gmail.com>
2024-11-20 21:20:56 -0300

    Add files via upload

--- db/instance.sql
+++ db/instance.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-09-2024 a las 01:56:33
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 13-11-2024 a las 18:53:42
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `subject`
+-- Base de datos: `db_school`
 --
 
 -- --------------------------------------------------------
@@ -29,13 +29,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `instance` (
   `id_Cicleinstance` int(11) NOT NULL,
-  `stdate_Instance` date NOT NULL,        -- Nueva columna
+  `stdate_Instance` date NOT NULL,
   `findate_Instance` date NOT NULL,
   `year_Instance` int(4) NOT NULL,
   `quarnumber_Instance` int(20) NOT NULL,
   `note_Instance` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `instance`
+--
+
+INSERT INTO `instance` (`id_Cicleinstance`, `stdate_Instance`, `findate_Instance`, `year_Instance`, `quarnumber_Instance`, `note_Instance`) VALUES
+(2, '2024-02-22', '2025-04-23', 2, 2, '2');
 
 --
 -- Índices para tablas volcadas
@@ -55,7 +61,7 @@ ALTER TABLE `instance`
 -- AUTO_INCREMENT de la tabla `instance`
 --
 ALTER TABLE `instance`
-  MODIFY `id_Cicleinstance` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_Cicleinstance` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- db/subject.sql
+++ db/subject.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-09-2024 a las 01:55:50
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 13-11-2024 a las 18:53:20
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `subject`
+-- Base de datos: `db_school`
 --
 
 -- --------------------------------------------------------
@@ -37,7 +37,15 @@ CREATE TABLE `subject` (
   `theme_Subject` varchar(30) NOT NULL,
   `note_Subject` decimal(3,0) NOT NULL,
   `turn_Subject` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `subject`
+--
+
+INSERT INTO `subject` (`id_Subject`, `name_Subject`, `year_Subject`, `teacher_Subject`, `level_Subject`, `hour_Subject`, `theme_Subject`, `note_Subject`, `turn_Subject`) VALUES
+(2, 'Matematica', 2, 'Mana', 3, 2, '1', 2, '3'),
+(3, 'sss', 2, 'd', 0, 1, 'a', 2, 'a');
 
 --
 -- Índices para tablas volcadas
@@ -57,7 +65,7 @@ ALTER TABLE `subject`
 -- AUTO_INCREMENT de la tabla `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `id_Subject` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_Subject` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
