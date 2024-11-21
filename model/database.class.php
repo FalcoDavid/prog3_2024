2b5959ba31d073d7d475acd0d9ef2d92e9bab14e
FalcoDavid <dfj.11.11.1972@gmail.com>
2024-11-20 21:20:12 -0300

    Add files via upload

--- model/database.class.php
+++ model/database.class.php
@@ -1,33 +1,35 @@
-<?php 
+<?php
     class Database{
+        // private $nombre="db_school";
+        // private $servidor="https://c197.ferozo.com:2092/extra_soft/phpMyAdmin/index.php";
+        // private $usuario="c1972251_school";
+        // private $clave="43ZEkigida";
+        // private $conexion;
+
         private $nombre="db_school";
         private $servidor="localhost";
         private $usuario="root";
         private $clave="";
-        private $conexion=null;
+        private $conexion;
 
-        //Conectar la base de datos 
+
+        //Conetar la base de datos
         public function __construct(){
-            $this ->conexion= new mysqli($this->servidor ,$this->usuario, $this->clave ,$this->nombre);
-            //es una variable que nos avisa si hay un error y si lo hay, nos dice "fallo la conexion".
+            $this->conexion= new mysqli($this->servidor ,$this->usuario ,$this->clave ,$this->nombre);
             if($this->conexion->connect_error){
-                print "Fallo la conexion:".$this->conexion->connect_error;
+                print "Fallo la conexion".$this->conexion->connect_error;
             }
         }
-    
-        //Ejecuta la consulta a la base de datos, por ejemplo: INSERT INTO, SELECT, DELETE, UPDATE...
-        public function query($sql) {
+        //Ejecuta la consulta
+        //Por ejemplo: INSERT INTO, SELECT, DELETE, UPDATE...
+        public function query($sql){
             return $this->conexion->query($sql);
         }
-
+        //Cierra la conexion a la base de daots.
         
-        //cierra la conexion de la base de datos.
-        //realizarla
         public function closeDB(){
             if($this->conexion !== null){
                 $this->conexion->close();
-                print "Conexion cerrada";
-                
             }
         }
     }
